b2c5dab4a78a290cc24d93c750c0df41fcbbdc3a
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-26 18:06:16 +0000

    Betty check

--- alias.c
+++ alias.c
@@ -10,18 +10,18 @@
 void add_alias(alias_t **alias_list, char *name, char *value)
 {
     alias_t *new_alias;
-
+    
     new_alias = malloc(sizeof(alias_t));
     if (new_alias == NULL)
     {
         write(STDERR_FILENO, "Error: failed to allocate memory for new alias.\n", 48);
         return;
     }
-
+    
     new_alias->name = strdup(name);
     new_alias->value = strdup(value);
     new_alias->next = *alias_list;
-
+    
     *alias_list = new_alias;
 }
 
@@ -33,17 +33,16 @@ void add_alias(alias_t **alias_list, char *name, char *value)
  */
 alias_t *find_alias(alias_t *alias_list, char *name)
 {
-alias_t *alias_ptr;
-
-for (alias_ptr = alias_list; alias_ptr != NULL; alias_ptr = alias_ptr->next)
-{
-if (strcmp(name, alias_ptr->name) == 0)
-{
-return (alias_ptr);
-}
-}
-
-return (NULL);
+    alias_t *alias_ptr;
+    for (alias_ptr = alias_list;
+         alias_ptr != NULL; alias_ptr = alias_ptr->next)
+    {
+        if (strcmp(name, alias_ptr->name) == 0)
+        {
+            return (alias_ptr);
+        }
+    }
+    return (NULL);
 }
 
 /**
@@ -54,28 +53,28 @@ return (NULL);
  */
 void delete_alias(alias_t **alias_list, char *name)
 {
-alias_t *alias_ptr;
-alias_t *prev_ptr;
-
-for (alias_ptr = *alias_list, prev_ptr = NULL;
-alias_ptr != NULL;
-prev_ptr = alias_ptr, alias_ptr = alias_ptr->next)
-{
-if (strcmp(name, alias_ptr->name) == 0)
-{
-if (prev_ptr == NULL)
-{
-*alias_list = alias_ptr->next;
-}
-else
-{
-prev_ptr->next = alias_ptr->next;
-}
-free(alias_ptr->name);
-free(alias_ptr->value);
-free(alias_ptr);
-return;
-}
-}
+    alias_t *alias_ptr;
+    alias_t *prev_ptr;
+    
+    for (alias_ptr = *alias_list, prev_ptr = NULL;
+         alias_ptr != NULL;
+         prev_ptr = alias_ptr, alias_ptr = alias_ptr->next)
+    {
+        if (strcmp(name, alias_ptr->name) == 0)
+        {
+            if (prev_ptr == NULL)
+            {
+                *alias_list = alias_ptr->next;
+            }
+            else
+            {
+                prev_ptr->next = alias_ptr->next;
+            }
+            free(alias_ptr->name);
+            free(alias_ptr->value);
+            free(alias_ptr);
+            return;
+        }
+    }
 }
 
