699024714b7ec2099441db55d30493909c5ec51f
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-26 19:11:14 +0000

    betty logop

--- logical_operators.c
+++ logical_operators.c
@@ -11,40 +11,40 @@ int execute(char **args);
  */
 int logical_operators(char *input)
 {
-    int status = 0;
-    int next_command = 1;
-    char *token = strtok(input, " ");
-    char *args[100];
-    int i = 0;
+int status = 0;
+int next_command = 1;
+char *token = strtok(input, " ");
+char *args[100];
+int i = 0;
 
-    while (token != NULL)
-    {
-        if (token[0] == '&' && token[1] == '&')
-        {
-            next_command = (status == 0) ? 1 : 0;
-        }
-        else if (token[0] == '|' && token[1] == '|')
-        {
-            next_command = (status != 0) ? 1 : 0;
-        }
-        else
-        {
-            if (next_command)
-            {
-                args[i++] = token;
-                args[i] = NULL;
-                status = execute(args);
-                next_command = 0;
-                i = 0;
-            }
-            else
-            {
-                args[i++] = token;
-                args[i] = NULL;
-            }
-        }
-        token = strtok(NULL, " ");
-    }
-    return status;
+while (token != NULL)
+{
+if (token[0] == '&' && token[1] == '&')
+{
+next_command = (status == 0) ? 1 : 0;
+}
+else if (token[0] == '|' && token[1] == '|')
+{
+next_command = (status != 0) ? 1 : 0;
+}
+else
+{
+if (next_command)
+{
+args[i++] = token;
+args[i] = NULL;
+status = execute(args);
+next_command = 0;
+i = 0;
+}
+else
+}
+args[i++] = token;
+args[i] = NULL;
+}
+}
+token = strtok(NULL, " ");
+}
+return (status);
 }
 
