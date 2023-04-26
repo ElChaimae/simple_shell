d3342813515d0ae771da47f72e7a5e89f9360539
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-26 19:02:41 +0000

    betty var

--- var_rep.c
+++ var_rep.c
@@ -19,18 +19,18 @@ void print_process_info(int, int);
  */
 char **parse_input(char *input)
 {
-    char **args = malloc(MAX_INPUT_SIZE / 2 + 1);
-    char *token;
-    int i = 0;
+char **args = malloc(MAX_INPUT_SIZE / 2 + 1);
+char *token;
+int i = 0;
 
-    token = strtok(input, " \t\n");
-    while (token != NULL)
-    {
-        args[i++] = token;
-        token = strtok(NULL, " \t\n");
-    }
-    args[i] = NULL;
-    return (args);
+token = strtok(input, " \t\n");
+while (token != NULL)
+{
+args[i++] = token;
+token = strtok(NULL, " \t\n");
+}
+args[i] = NULL;
+return (args);
 }
 
 /**
@@ -40,24 +40,23 @@ char **parse_input(char *input)
  */
 int handle_built_in_commands(char **args)
 {
-    if (args[0] == NULL)
-    {
-        return (0);
-    }
-    if (strcmp(args[0], "exit") == 0)
-    {
-        exit(0);
-    }
-    else if (strcmp(args[0], "cd") == 0)
-    {
-        if (chdir(args[1]) != 0)
-        {
-            perror("cd");
-        }
-        return (0);
-    }
-
-    return (-1);
+if (args[0] == NULL)
+{
+return (0);
+}
+if (strcmp(args[0], "exit") == 0)
+{
+exit(0);
+}
+else if (strcmp(args[0], "cd") == 0)
+{
+if (chdir(args[1]) != 0)
+{
+perror("cd");
+}
+return (0);
+}
+return (-1);
 }
 
 
@@ -69,11 +68,11 @@ int handle_built_in_commands(char **args)
  */
 void print_process_info(int pid, int status)
 {
-    char pid_str[20], status_str[20];
+char pid_str[20], status_str[20];
 
-    sprintf(pid_str, "$$: %d\n", pid);
-    write(STDOUT_FILENO, pid_str, strlen(pid_str));
-    sprintf(status_str, "$?: %d\n", status);
-    write(STDOUT_FILENO, status_str, strlen(status_str));
+sprintf(pid_str, "$$: %d\n", pid);
+write(STDOUT_FILENO, pid_str, strlen(pid_str));
+sprintf(status_str, "$?: %d\n", status);
+write(STDOUT_FILENO, status_str, strlen(status_str));
 }
 
