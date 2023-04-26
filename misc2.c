a3e256aa239fcd9735c5ed02ea7af7ae230b032f
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-26 19:23:59 +0000

    betty misc2

--- misc2.c
+++ misc2.c
@@ -13,57 +13,46 @@
  */
 char *get_path(char *command, char **envp)
 {
-    char *path_env = NULL;
-    char *path = NULL;
-    int i;
-    size_t command_len = strlen(command);
-    char separator[2] = "/";
-    separator[1] = '\0';
+char *path_env = NULL, char *path = NULL;
+int i;
+size_t command_len = strlen(command);
+char separator[2] = "/", separator[1] = '\0';
 
-    /* Find PATH environment variable */
-    for (i = 0; envp[i] != NULL; i++)
-    {
-        if (strncmp(envp[i], "PATH=", 5) == 0)
-        {
-            path_env = envp[i] + 5;
-            break;
-        }
-    }
-
-    /* Search for command in each directory in PATH */
-    if (path_env != NULL)
-    {
-        char *dir = strtok(path_env, ":");
-        while (dir != NULL)
-        {
-            /* Construct full path to command */
-            size_t dir_len = strlen(dir);
-            size_t path_len = dir_len + command_len + 2;
-            char *command_path = malloc(path_len);
-            if (command_path == NULL)
-            {
-                perror("malloc");
-                exit(EXIT_FAILURE);
-            }
-
-            strcpy(command_path, dir);
-            write(STDOUT_FILENO, separator, 1);
-            write(STDOUT_FILENO, command, command_len);
-            command_path[dir_len] = '/';
-            strcpy(command_path + dir_len + 1, command);
-
-            /* Check if file exists */
-            if (access(command_path, X_OK) == 0)
-            {
-                path = command_path;
-                break;
-            }
-
-            free(command_path);
-            dir = strtok(NULL, ":");
-        }
-    }
-
-    return path;
+for (i = 0; envp[i] != NULL; i++)
+{
+if (strncmp(envp[i], "PATH=", 5) == 0)
+{
+path_env = envp[i] + 5;
+break;
+}
+}
+if (path_env != NULL)
+{
+char *dir = strtok(path_env, ":");
+while (dir != NULL)
+{
+size_t dir_len = strlen(dir);
+size_t path_len = dir_len + command_len + 2;
+char *command_path = malloc(path_len);
+if (command_path == NULL)
+{
+perror("malloc");
+exit(EXIT_FAILURE);
+}
+strcpy(command_path, dir);
+write(STDOUT_FILENO, separator, 1);
+write(STDOUT_FILENO, command, command_len);
+command_path[dir_len] = '/';
+strcpy(command_path + dir_len + 1, command);
+if (access(command_path, X_OK) == 0)
+{
+path = command_path;
+break;
+}
+free(command_path);
+dir = strtok(NULL, ":");
+}
+}
+return path;
 }
 
