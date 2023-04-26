9a8df048f1302e2ba4185f0ba857d910f53d12b5
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-26 18:51:58 +0000

    betty exit

--- exit_shell.c
+++ exit_shell.c
@@ -11,24 +11,27 @@
  * @str: string to convert
  * Return: corresponding long integer value
  */
-long str_to_long(const char *str) {
-    long result = 0;
-    int sign = 1;
-    const char *p = str;
+long str_to_long(const char *str)
+{
+long result = 0;
+int sign = 1;
+const char *p = str;
 
-    if (*p == '-') {
-        sign = -1;
-        p++;
-    } else if (*p == '+') {
-        p++;
-    }
-
-    while (*p >= '0' && *p <= '9') {
-        result = result * 10 + (*p - '0');
-        p++;
-    }
-
-    return sign * result;
+if (*p == '-')
+{
+sign = -1;
+p++;
+}
+else if (*p == '+')
+}
+p++;
+}
+while (*p >= '0' && *p <= '9')
+{
+result = result * 10 + (*p - '0');
+p++;
+}
+return (sign * result);
 }
 
 /**
@@ -38,46 +41,40 @@ long str_to_long(const char *str) {
  */
 int my_atoi(const char *s)
 {
-  int result = 0;
-  int sign = 1;
-  int i = 0;
+int result = 0;
+int sign = 1;
+int i = 0;
 
-  if (s == NULL || *s == '\0')
-  {
-    char *error_message = "Error: input string is empty or NULL.\n";
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    return -1;
-  }
-
-  if (*s == '-')
-  {
-    sign = -1;
-    i++;
-  }
-
-  while (s[i] != '\0')
-  {
-    if (!isdigit(s[i]))
-    {
-      char *error_message = "Error: input string contains non-digit characters.\n";
-      write(STDERR_FILENO, error_message, strlen(error_message));
-      return -1;
-    }
-
-    result = result * 10 + (s[i] - '0');
-    i++;
-  }
-
-  result *= sign;
-
-  if (result < 0 || result > 255)
-  {
-    char *error_message = "Error: converted value is out of range.\n";
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    return -1;
-  }
-
-  return result;
+if (s == NULL || *s == '\0')
+{
+char *error_message = "Error: input string is empty or NULL.\n";
+write(STDERR_FILENO, error_message, strlen(error_message));
+return (-1);
+}
+if (*s == '-')
+{
+sign = -1;
+i++;
+}
+while (s[i] != '\0')
+{
+if (!isdigit(s[i]))
+{
+char *error_message = "Error: input string contains non-digit characters.\n";
+write(STDERR_FILENO, error_message, strlen(error_message));
+return (-1);
+}
+result = result * 10 + (s[i] - '0');
+i++;
+}
+result *= sign;
+if (result < 0 || result > 255)
+{
+char *error_message = "Error: converted value is out of range.\n";
+write(STDERR_FILENO, error_message, strlen(error_message));
+return (-1);
+}
+return (result);
 }
 
 /**
@@ -87,33 +84,33 @@ int my_atoi(const char *s)
  */
 int exit_shell(char **args)
 {
-  char *message;
+char *message;
 
-  if (args[1] == NULL)
-  {
-    message = "Exiting Shell...\n";
-    write(STDOUT_FILENO, message, strlen(message));
-    exit(EXIT_SUCCESS);
-  }
-  else if (args[2] != NULL)
-  {
-    char *error_message = "Usage: exit [status]\n";
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    return (EXIT_FAILURE);
-  }
-  else
-  {
-    long status = str_to_long(args[1]);
-    if (status < 0 || status > 255)
-    {
-      char error_message[100];
-      size_t len = snprintf(error_message, sizeof(error_message), "Invalid exit status: %s\n", args[1]);
-      write(STDERR_FILENO, error_message, len);
-      return (EXIT_FAILURE);
-    }
-    message = "Exiting Shell...\n";
-    write(STDOUT_FILENO, message, strlen(message));
-    exit((int) status);
-  }
+if (args[1] == NULL)
+{
+message = "Exiting Shell...\n";
+write(STDOUT_FILENO, message, strlen(message));
+exit(EXIT_SUCCESS);
+}
+else if (args[2] != NULL)
+{
+char *error_message = "Usage: exit [status]\n";
+write(STDERR_FILENO, error_message, strlen(error_message));
+return (EXIT_FAILURE);
+}
+else
+{
+long status = str_to_long(args[1]);
+if (status < 0 || status > 255)
+{
+char error_message[100];
+size_t len = snprintf(error_message, sizeof(error_message), "Invalid exit status: %s\n", args[1]);
+write(STDERR_FILENO, error_message, len);
+return (EXIT_FAILURE);
+}
+message = "Exiting Shell...\n";
+write(STDOUT_FILENO, message, strlen(message));
+exit((int) status);
+}
 }
 
