24ed14f6d83f5a6c2dc89ec792f80613c0ab60e4
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-26 19:14:50 +0000

    berry main

--- main.c
+++ main.c
@@ -11,38 +11,38 @@
  */
 int main(void)
 {
-    char buffer[BUFFER_SIZE];
-    char *args[100];
-    int arg_count;
-    char *arg;
+char buffer[BUFFER_SIZE];
+char *args[100];
+int arg_count;
+char *arg;
 
-    while (1)
-    {
-        if (write(STDOUT_FILENO, "$ ", 2) == -1)
-        {
-            perror("write");
-            exit(EXIT_FAILURE);
-        }
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
-        {
-            break;
-        }
-
-        /*Parse the user input into separate arguments*/
-        arg_count = 0;
-        arg = strtok(buffer, " \t\n");
-        while (arg != NULL && arg_count < 99)
-        {
-            args[arg_count] = arg;
-            arg_count++;
-            arg = strtok(NULL, " \t\n");
-        }
-        args[arg_count] = NULL;
-        /*Execute the command with the parsed arguments*/
-        if (arg_count > 0)
-        {
-            execute_command(args);
-        }
-    }
-    exit(EXIT_SUCCESS);
+while (1)
+{
+if (write(STDOUT_FILENO, "$ ", 2) == -1)
+{
+perror("write");
+exit(EXIT_FAILURE);
 }
+if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+{
+break;
+}
+/*Parse the user input into separate arguments*/
+arg_count = 0;
+arg = strtok(buffer, " \t\n");
+while (arg != NULL && arg_count < 99)
+{
+args[arg_count] = arg;
+arg_count++;
+arg = strtok(NULL, " \t\n");
+}
+args[arg_count] = NULL;
+/*Execute the command with the parsed arguments*/
+if (arg_count > 0)
+{
+execute_command(args);
+}
+}
+exit(EXIT_SUCCESS);
+}
+
