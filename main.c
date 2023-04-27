6fea39864950ebb9ee16ec8e5498078d866c9246
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-27 13:26:26 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 #include "main.h"
 
 #define BUFFER_SIZE 1024
@@ -15,7 +17,10 @@ char buffer[BUFFER_SIZE];
 char *args[100];
 int arg_count;
 char *arg;
-
+/* Register signal handler*/
+signal(SIGINT, handle_sigint);
+/* Set the PATH environment variable */
+printf("PATH set successfully\n");
 while (1)
 {
 if (write(STDOUT_FILENO, "$ ", 2) == -1)
@@ -44,5 +49,14 @@ execute_command(args);
 }
 }
 exit(EXIT_SUCCESS);
+}
+/**
+ * handle_sigint - a function that handles the SIGINT signal
+ */
+void handle_sigint(int sig_num)
+{
+(void) sig_num;
+fflush(stdout);
+write(STDOUT_FILENO, "\n$ ", 3);
 }
 
