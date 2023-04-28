c1dcb3d379c62c04fa4d79617c9c5f7dc406a93b
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-04-28 16:42:03 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,4 +1,11 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIM " \t\r\n\a"
 
 /**
  * read_input - Reads a line of input from stdin and stores it in memory.
@@ -9,15 +16,16 @@
  */
 int read_input(char **input_line, size_t *input_size)
 {
-	int read_result = getline(input_line, input_size, stdin);
+    ssize_t read_result = getline(input_line, input_size, stdin);
 
-	if (read_result == -1)
-	{
-		if (feof(stdin))
-			return (-1);
-		exit(EXIT_FAILURE);	
-	}
-	return (0);
+    if (read_result == -1)
+    {
+        if (feof(stdin))
+            return (-1);
+        perror("Error reading input");
+        exit(EXIT_FAILURE);
+    }
+    return (0);
 }
 
 /**
@@ -27,19 +35,19 @@ int read_input(char **input_line, size_t *input_size)
  */
 int print_prompt(void)
 {
-	char *prompt_txt = "$ ";
-	int write_result; 
-	
-	if(isatty(STDIN_FILENO))
-	{
-		write_result = write(STDOUT_FILENO, prompt_txt, strlen(prompt_txt));
-		if (write_result == -1)
-		{
-			perror("Error writing prompt to stdout");
-			return (-1);
-		}
-	}
-	return (0);
+    char *prompt_txt = "$ ";
+    ssize_t write_result;
+
+    if (isatty(STDOUT_FILENO))
+    {
+        write_result = write(STDOUT_FILENO, prompt_txt, strlen(prompt_txt));
+        if (write_result == -1)
+        {
+            perror("Error writing prompt to stdout");
+            return (-1);
+        }
+    }
+    return (0);
 }
 
 /**
@@ -50,27 +58,39 @@ int print_prompt(void)
  */
 char **tokenize(char *input)
 {
-	int bufferSize = 10;
-	char **args = malloc(bufferSize * sizeof(char *));
-	char *token;
-	int count = 0;
+    int bufsize = TOKEN_BUFSIZE;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+    int pos = 0;
 
-	token = strtok(input, " ");
-	args[count++] = token;
+    if (!tokens)
+    {
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
+    }
 
-	while (token != NULL)
-	{
-		if (count >= bufferSize)
-		{
-			bufferSize *= 2;
-			args = realloc(args, bufferSize * sizeof(char *));
-		}
-		token = strtok(NULL, " ");
-		args[count++] = token;
-	}
-	return (args);
+    token = strtok(input, TOKEN_DELIM);
+    while (token != NULL)
+    {
+        tokens[pos] = token;
+        pos++;
+
+        if (pos >= bufsize)
+        {
+            bufsize += TOKEN_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOKEN_DELIM);
+    }
+    tokens[pos] = NULL;
+    return tokens;
 }
-
 
 /**
  * exec_cmd - Execute a command with given arguments using execve.
@@ -80,29 +100,35 @@ char **tokenize(char *input)
  */
 int exec_cmd(char **args)
 {
-	pid_t pid;
-	int status;
+    pid_t pid;
+    int status;
 
-	if (!*args || !args[0])
-	{
-		return (-1);
-	}
-	if (strcmp(args[0], "exit") == 0)
-		exit(EXIT_FAILURE);
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Fork error");
-		return (-1);
-	}
-	else if (pid == 0)
-	{
-		execve(args[0], args, environ);
-		perror(args[0]);
-		exit(EXIT_FAILURE);
-	}
-	else
-		waitpid(pid, &status, 0);
-
-	return (0);
+    if (!*args || !args[0])
+    {
+        return (-1);
+    }
+    if (strcmp(args[0], "exit") == 0)
+        exit(EXIT_SUCCESS);
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("Fork error");
+        return (-1);
+    }
+    else if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("Exec error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        do
+        {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return (0);
 }
