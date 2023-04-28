8aa725be7a2e6cd292fbf34fc4bb42eaeda07a73
DzidediSenaya <dzidedisenaya@gmail.com>
2023-04-28 12:39:12 -0700

    modifications

--- execute_command.c
+++ execute_command.c
@@ -1,35 +1,40 @@
 #include "main.h"
 
+void execute_child(char **args);
+void wait_for_child(pid_t pid, int *status);
+
+
 /**
- *  execute_left_command - handles left pipe
- *  @args: arguments passed
- *  @pipefd: array for creating a pipe between two processes
- *  Return: 0
+ * execute_left_command - handles left pipe
+ * @args: arguments passed
+ * @pipefd: array for creating a pipe between two processes
+ * Return: 0
+ */
 void execute_left_command(char **args, int pipefd[])
 {
-pid_t pid;
-int status;
+    pid_t pid;
+    int status;
 
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-exit(EXIT_FAILURE);
-} 
-else if (pid == 0)
-{
-/*Set up pipe*/
-close(pipefd[0]);
-dup2(pipefd[1], STDOUT_FILENO);
-close(pipefd[1]);
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } 
+    else if (pid == 0)
+    {
+        /*Set up pipe*/
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
 
-/*Execute command*/
-execute_child(args);
-}
-/* Close pipe write end*/
-close(pipefd[1]);
-/*Wait for child process to finish*/
-wait_for_child(pid, &status);
+        /*Execute command*/
+        execute_child(args);
+    }
+    /* Close pipe write end*/
+    close(pipefd[1]);
+    /*Wait for child process to finish*/
+    wait_for_child(pid, &status);
 }
 
 /**
@@ -40,28 +45,28 @@ wait_for_child(pid, &status);
  */
 void execute_right_command(char **args, int pipefd[])
 {
-pid_t pid;
-int status;
+    pid_t pid;
+    int status;
 
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-exit(EXIT_FAILURE);
-}
-else if (pid == 0)
-{
-/*Set up pipe*/
-close(pipefd[1]);
-dup2(pipefd[0], STDIN_FILENO);
-close(pipefd[0]);
-/*Execute command*/
-execute_child(args);
-}
-/* Close pipe read end*/
-close(pipefd[0]);
-/* Wait for child process to finish*/
-wait_for_child(pid, &status);
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        /*Set up pipe*/
+        close(pipefd[1]);
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
+        /*Execute command*/
+        execute_child(args);
+    }
+    /* Close pipe read end*/
+    close(pipefd[0]);
+    /* Wait for child process to finish*/
+    wait_for_child(pid, &status);
 }
 
 /**
@@ -71,14 +76,16 @@ wait_for_child(pid, &status);
  */
 int check_for_pipe(char **args)
 {
-for (int i = 0; args[i] != NULL; i++)
-{
-if (strcmp(args[i], "|") == 0)
-{
-return (i);
-}
-}
-return (-1);
+int i;
+
+    for (i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "|") == 0)
+        {
+            return (i);
+        }
+    }
+    return (-1);
 }
 
 /**
@@ -88,52 +95,53 @@ return (-1);
  */
 void execute_single_command(char **args)
 {
-pid_t pid;
-int status;
+    pid_t pid;
+    int status;
 
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-exit(EXIT_FAILURE);
-}
-else if (pid == 0)
-{
-/*Execute command*/
-execute_child(args);
-}
-/*Wait for child process to finish*/
-wait_for_child(pid, &status);
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        /*Execute command*/
+        execute_child(args);
+    }
+    /*Wait for child process to finish*/
+    wait_for_child(pid, &status);
 }
 
 /**
- * execute_command - executes a command
+ * execute_command - handles command execution
  * @args: arguments passed
  * Return: 0
  */
- int execute_command(char **args)
+int execute_command(char **args)
 {
-int pipe_index = check_for_pipe(args);
-if (pipe_index == -1)
-{
-/*No pipe found, execute single command*/
-execute_single_command(args);
-}
-else
-{
-/*Pipe found, execute left and right commands*/
-int pipefd[2];
-if (pipe(pipefd) == -1)
-{
-perror("pipe");
-return (0);
-}
-/ *Execute left command*/
-args[pipe_index] = NULL;
-execute_left_command(args, pipefd);
-/*Execute right command*/
-execute_right_command(&args[pipe_index+1], pipefd);
-}
-return (1);
+    int pipe_pos = check_for_pipe(args);
+    int pipefd[2];
+
+    if (pipe_pos == -1)
+    {
+        execute_single_command(args);
+    }
+    else
+    {
+        /* Create pipe */
+        if (pipe(pipefd) == -1)
+        {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+
+        /* Execute left command */
+        execute_left_command(args, pipefd);
+
+        /* Execute right command */
+        execute_right_command(&args[pipe_pos + 1], pipefd);
+    }
+    return (0);
 }
 
--- main.h
+++ main.h
@@ -1,17 +1,70 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MAX_INPUT_LENGTH 1024
+#define TOKEN_BUFSIZE 64
+
 extern char **environ;
+
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <sys/stat.h>
 
+/* struct for aliases */
+typedef struct alias {
+    char *name;
+    char *value;
+    struct alias *next;
+} alias_t;
+
+/* string functions */
+char *_strdup(char *str);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strchr(char *s, char c);
+
+/* token functions */
+char **tokenize(char *input);
+void free_tokens(char **tokens);
+
+/* command functions */
+int is_exit_command(char *command);
+int is_cd_command(char **tokens);
+int execute_command(char **args);
+void execute_cd_command(char* dir);
+char *get_command_path(char *command, char **envp);
+void print_command_not_found(char *command);
+void print_error(char *command);
+
+/* alias functions */
+alias_t *create_alias(char *name, char *value);
+void add_alias(alias_t **alias_list, char *name, char *value);
+void print_aliases(alias_t *alias_list);
+alias_t *get_alias(alias_t *alias_list, char *name);
+void free_aliases(alias_t **alias_list);
+
+/*other functions*/
+int execute_cmd(char **args);
+void handle_sigint(int sig_num);
+int exit_shell(char **args);
+int execute(char **args);
 int read_input(char **input_line, size_t *input_size);
 int print_prompt(void);
 char** tokenize(char *input);
 int exec_cmd(char **args, int *pipefd);
+void execute_child(char **args);
+void wait_for_child(pid_t pid, int *status);
 
-#endif
+
+#endif /* MAIN_H */
