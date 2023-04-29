664a8c914a2a60743397574112c3570fd2322997
DzidediSenaya <dzidedisenaya@gmail.com>
2023-04-29 15:59:31 -0700

    main

--- main.h
+++ main.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-char* run_command(char* input);
+int run_command(char* input);
 void free_args(char** args);
-char** parse_args(char* input);
 int main(int argc, char* argv[]);
 int execute_command(char** args);
+char** parse_args(char* input);
 
 #endif /* MAIN_H_ */
 
