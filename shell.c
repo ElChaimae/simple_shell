8e2f301b6a46cefc0b6045ba23f44305143eb561
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-05-24 16:54:45 -0700

    Fix betty errors

--- shell.c
+++ shell.c
@@ -4,64 +4,74 @@
 #include <string.h>
 #include <unistd.h>
 
-void write_stdout(const char* message, size_t len) {
-    write(STDOUT_FILENO, message, len);
+void write_stdout(const char *message, size_t len)
+{
+write(STDOUT_FILENO, message, len);
 }
 
-void write_stderr(const char* message, size_t len) {
-    write(STDERR_FILENO, message, len);
+void write_stderr(const char *message, size_t len)
+{
+write(STDERR_FILENO, message, len);
 }
 
 
-char** parse_args(char* input) {
-    char** args = malloc((MAX_ARGS + 1) * sizeof(char*));
-    int arg_count = 0;
-    char *arg;
+char **parse_args(char *input)
+{
+char **args = malloc((MAX_ARGS + 1) * sizeof(char *));
+int arg_count = 0;
+char *arg;
 
-    if (!args) {
-        perror("malloc");
-        exit(1);
-    }
-
-    while (*input) {
-        while (*input && (*input == ' ' || *input == '\t'))
-            input++;
-
-        if (*input == '\0' || *input == '\n')
-            break;
-
-        if (*input == '"') {
-            input++;
-            arg = input;
-            while (*input && *input != '"')
-                input++;
-        } else {
-            arg = input;
-            while (*input && *input != ' ' && *input != '\t')
-                input++;
-        }
-
-        if (*input)
-            *input++ = '\0';
-
-        args[arg_count++] = strdup(arg);
-
-        if (arg_count >= MAX_ARGS)
-            break;
-    }
-
-    args[arg_count] = NULL;
-    return args;
+if (!args)
+{
+perror("malloc");
+exit(1);
 }
 
-void free_args(char** args) {
-    int i;
+while (*input)
+{
+while (*input && (*input == ' ' || *input == '\t'))
+input++;
 
-    if (args) {
-        for (i = 0; args[i]; i++) {
-            free(args[i]);
-        }
-        free(args);
-    }
+if (*input == '\0' || *input == '\n')
+break;
+
+if (*input == '"')
+{
+input++;
+arg = input;
+while (*input && *input != '"')
+input++;
+}
+else
+{
+arg = input;
+while (*input && *input != ' ' && *input != '\t')
+input++;
+}
+
+if (*input)
+*input++ = '\0';
+
+args[arg_count++] = strdup(arg);
+
+if (arg_count >= MAX_ARGS)
+break;
+}
+
+args[arg_count] = NULL;
+return (args);
+}
+
+void free_args(char **args)
+{
+int i;
+
+if (args)
+{
+for (i = 0; args[i]; i++)
+free(args[i]);
+
+free(args);
+}
 }
 
--- shell2.c
+++ shell2.c
@@ -8,156 +8,186 @@
 
 extern char **environ;
 
-int execute_command(char** args) {
-    int status;
-    pid_t pid = fork();
+int execute_command(char **args)
+{
+int status;
+pid_t pid = fork();
 
-    if (pid == -1) {
-        perror("fork");
-        return -1;
-    } else if (pid == 0) {
-        execve(args[0], args, environ);
-        perror("execve");
-        exit(127);
-    } else {
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
-            return WEXITSTATUS(status);
-        else
-            return -1;
-    }
+if (pid == -1)
+{
+perror("fork");
+return (-1);
+}
+else if (pid == 0)
+{
+execve(args[0], args, environ);
+perror("execve");
+exit(127);
+}
+else
+{
+waitpid(pid, &status, 0);
+if (WIFEXITED(status))
+return (WEXITSTATUS(status));
+else
+return (-1);
+}
 }
 
+int run_command(char *input, alias_t **alias_list)
+{
+char **args;
+int result;
+char *path;
+char *dir;
+char command_path[1024];
+int command_found = 0;
+char **env_value;
+int exit_status = 0;
+alias_t *alias;
 
+/* Parse arguments */
+args = parse_args(input);
 
-#include <stdlib.h>
-#include <string.h>
+/* Handle special commands */
+if (!args[0])
+{
+free_args(args);
+return (0);
+}
 
-int run_command(char* input, alias_t** alias_list) {
-    char** args;
-    int result;
-    char* path;
-    char* dir;
-    char command_path[1024];
-    int command_found = 0;
-    char** env_value;
-    int exit_status = 0;
-    alias_t* alias;
+/* Check if the entered command is an alias */
+alias = find_alias(alias_list, args[0]);
+if (alias != NULL)
+{
+/* Replace the command with the alias value */
+free(args[0]);
+args[0] = strdup(alias->value);
+}
 
-    /* Parse arguments */
-    args = parse_args(input);
+if (strcmp(args[0], "exit") == 0)
+{
 
-    /* Handle special commands */
-    if (!args[0]) {
-        free_args(args);
-        return 0;
-    }
+if (args[1])
+exit_status = atoi(args[1]);
 
-    /* Check if the entered command is an alias */
-    alias = find_alias(alias_list, args[0]);
-    if (alias != NULL) {
-        /* Replace the command with the alias value */
-        free(args[0]);
-        args[0] = strdup(alias->value);
-    }
+free_args(args);
+exit(exit_status);
+}
+else if (strcmp(args[0], "env") == 0)
+{
+/* Print environment variables */
+for (env_value = environ; *env_value; env_value++)
+{
+write_stdout(*env_value, strlen(*env_value));
+write_stdout("\n", 1);
+}
+free_args(args);
+return (0);
+}
+else if (strcmp(args[0], "setenv") == 0)
+{
+/* Set environment variable */
+if (args[1] && args[2])
+{
+if (setenv(args[1], args[2], 1) == -1)
+perror("setenv");
 
-    if (strcmp(args[0], "exit") == 0) {
-        
-	if (args[1]) {
-            exit_status = atoi(args[1]);
-        }
-        free_args(args);
-        exit(exit_status);
-    } else if (strcmp(args[0], "env") == 0) {
-        /* Print environment variables */
-        for (env_value = environ; *env_value; env_value++) {
-            write_stdout(*env_value, strlen(*env_value));
-            write_stdout("\n", 1);
-        }
-        free_args(args);
-        return 0;
-    } else if (strcmp(args[0], "setenv") == 0) {
-        /* Set environment variable */
-        if (args[1] && args[2]) {
-            if (setenv(args[1], args[2], 1) == -1) {
-                perror("setenv");
-            }
-        } else {
-            write_stderr("setenv: Invalid syntax. Usage: setenv VARIABLE VALUE\n", 54);
-        }
-        free_args(args);
-        return 0;
-    } else if (strcmp(args[0], "unsetenv") == 0) {
-        /* Unset environment variable */
-        if (args[1]) {
-            if (unsetenv(args[1]) == -1) {
-                perror("unsetenv");
-            }
-        } else {
-            write_stderr("unsetenv: Invalid syntax. Usage: unsetenv VARIABLE\n", 51);
-        }
-        free_args(args);
-        return 0;
-    } else if (strcmp(args[0], "cd") == 0) {
-        /* Change directory */
-        result = cd(args);
-        free_args(args);
-        return result;
-    }
+}
+else
+write_stderr("setenv: Invalid syntax. Usage: setenv VARIABLE VALUE\n", 54);
 
-    /* Check if the command is an executable file */
-    if (args[0][0] == '.' || args[0][0] == '/' || args[0][0] == '~') {
-        if (access(args[0], X_OK) == 0) {
-            command_found = 1;
-        } else {
-            write_stderr("./hsh: 1: ", 13);
-            write_stderr(args[0], strlen(args[0]));
-            write_stderr(": not found\n", 12);
-            free_args(args);
-            return -1;
-        }
-    } else {
-        /* Search for the command in the directories specified by PATH */
-        path = getenv("PATH");
-        if (path == NULL) {
-            write_stderr("./hsh: 1: ", 10);
-            write_stderr(args[0], strlen(args[0]));
-            write_stderr(": not found\n", 12);
-            free_args(args);
-            return 127;
-        }
-        dir = strtok(path, ":");
-        while (dir && !command_found) {
-            snprintf(command_path, sizeof(command_path), "%s/%s", dir, args[0]);
-            if (access(command_path, X_OK) == 0) {
-                args[0] = strdup(command_path);
-                command_found = 1;
-            } else {
-                /* Add condition to check if access fails for the original command */
-                snprintf(command_path, sizeof(command_path), "/bin/%s", args[0]);
-                if (access(command_path, X_OK) == 0) {
-                    args[0] = strdup(command_path);
-                    command_found = 1;
-                } else {
-                    dir = strtok(NULL, ":");
-                }
-            }
-        }
-    }
+free_args(args);
+return (0);
+}
+else if (strcmp(args[0], "unsetenv") == 0)
+{
+/* Unset environment variable */
+if (args[1])
+{
+if (unsetenv(args[1]) == -1)
+perror("unsetenv");
 
-    /* Execute the command */
-    if (!command_found) {
-        write_stderr("./hsh: 1: ", 10);
-        write_stderr(args[0], strlen(args[0]));
-        write_stderr(": not found\n", 12);
-        free_args(args);
-        return 127;
-    }
+}
+else
+write_stderr("unsetenv: Invalid syntax. Usage: unsetenv VARIABLE\n", 51);
 
-    result = execute_command(args);
+free_args(args);
+return (0);
+}
+else if (strcmp(args[0], "cd") == 0)
+{
+/* Change directory */
+result = cd(args);
+free_args(args);
+return (result);
+}
 
-    free_args(args);
-    return result;
+/* Check if the command is an executable file */
+if (args[0][0] == '.' || args[0][0] == '/' || args[0][0] == '~')
+{
+if (access(args[0], X_OK) == 0)
+command_found = 1;
+else
+{
+write_stderr("./hsh: 1: ", 13);
+write_stderr(args[0], strlen(args[0]));
+write_stderr(": not found\n", 12);
+free_args(args);
+return (-1);
+}
+}
+else
+{
+/* Search for the command in the directories specified by PATH */
+path = getenv("PATH");
+if (path == NULL)
+{
+write_stderr("./hsh: 1: ", 10);
+write_stderr(args[0], strlen(args[0]));
+write_stderr(": not found\n", 12);
+free_args(args);
+return (127);
+}
+dir = strtok(path, ":");
+while (dir && !command_found)
+{
+snprintf(command_path, sizeof(command_path), "%s/%s", dir, args[0]);
+if (access(command_path, X_OK) == 0)
+{
+args[0] = strdup(command_path);
+command_found = 1;
+}
+else
+{
+/* Add condition to check if access fails for the original command */
+snprintf(command_path, sizeof(command_path), "/bin/%s", args[0]);
+if (access(command_path, X_OK) == 0)
+{
+args[0] = strdup(command_path);
+command_found = 1;
+}
+else
+dir = strtok(NULL, ":");
+
+}
+}
+}
+
+/* Execute the command */
+if (!command_found)
+{
+write_stderr("./hsh: 1: ", 10);
+write_stderr(args[0], strlen(args[0]));
+write_stderr(": not found\n", 12);
+free_args(args);
+return (127);
+}
+
+result = execute_command(args);
+
+free_args(args);
+return (result);
 }
 
 
