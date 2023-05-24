5e27c3de38a46180da1555e1d530808c00e3b77f
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-05-24 14:14:35 -0700

    Fix betty errors on log_ops.c

--- log_ops.c
+++ log_ops.c
@@ -11,39 +11,39 @@ int execute_command(char **args);
  */
 int logical_operators(char *input)
 {
-    int status = 0;
-    int next_command = 1;
-    char *token;
-    char **args = NULL;
-    const char *delimiters = " \t\n";
+int status = 0;
+int next_command = 1;
+char *token;
+char **args = NULL;
+const char *delimiters = " \t\n";
 
-    token = strtok(input, delimiters);
-    while (token != NULL)
-    {
-        if (strcmp(token, "&&") == 0)
-        {
-            next_command = (status == 0) ? 1 : 0;
-        }
-        else if (strcmp(token, "||") == 0)
-        {
-            next_command = (status != 0) ? 1 : 0;
-        }
-        else
-        {
-            if (next_command)
-            {
-                args = realloc(args, sizeof(char *) * 2);
-                args[0] = token;
-                args[1] = NULL;
-                status = execute_command(args);
-                next_command = 0;
-            }
-        }
-        token = strtok(NULL, delimiters);
-    }
+token = strtok(input, delimiters);
+while (token != NULL)
+{
+if (strcmp(token, "&&") == 0)
+{
+next_command = (status == 0) ? 1 : 0;
+}
+else if (strcmp(token, "||") == 0)
+{
+next_command = (status != 0) ? 1 : 0;
+}
+else
+{
+if (next_command)
+{
+args = realloc(args, sizeof(char *) * 2);
+args[0] = token;
+args[1] = NULL;
+status = execute_command(args);
+next_command = 0;
+}
+}
+token = strtok(NULL, delimiters);
+}
 
-    free(args);
+free(args);
 
-    return status;
+return (status);
 }
 
