58a85d1cad28ff4d11818c2e2bb7461e1cde5543
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-05-24 13:41:30 -0700

    Fix betty errors on command_seperator.c

--- command_separator.c
+++ command_separator.c
@@ -12,50 +12,50 @@
  */
 int command_separator(char *input)
 {
-    char *saveptr;
-    char *token;
-    int child_status, status = 0;
-    
-    token = strtok_r(input, ";", &saveptr);
-    while (token != NULL)
-    {
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            perror("fork");
-            exit(1);
-        }
-        else if (pid == 0) /* child process */
-        {
-            /* Tokenize the command to separate command name and arguments */
-            char *args[64];  /* Assuming maximum 63 arguments + NULL terminator */
-            int arg_count = 0;
-            
-            args[arg_count++] = strtok(token, " ");
-            while (arg_count < 63 && args[arg_count-1] != NULL)
-            {
-                args[arg_count++] = strtok(NULL, " ");
-            }
-            args[arg_count] = NULL;  /* Add NULL terminator */
-            
-            if (execvp(args[0], args) == -1)
-            {
-                perror("execvp");
-                exit(1);  /* Exit child process with error status */
-            }
-        }
-        else /* parent process */
-        {
-            waitpid(pid, &child_status, 0); /* wait for child to finish */
-            if (WIFEXITED(child_status) && WEXITSTATUS(child_status) != 0)
-            {
-                status = 1; /* set status to 1 if child process fails */
-            }
-        }
-        
-        token = strtok_r(NULL, ";", &saveptr); /* move to next token */
-    }
-    
-    return status;
+char *saveptr;
+char *token;
+int child_status, status = 0;
+
+token = strtok_r(input, ";", &saveptr);
+while (token != NULL)
+{
+pid_t pid = fork();
+if (pid < 0)
+{
+perror("fork");
+exit(1);
+}
+else if (pid == 0) /* child process */
+{
+/* Tokenize the command to separate command name and arguments */
+char *args[64];  /* Assuming maximum 63 arguments + NULL terminator */
+int arg_count = 0;
+
+args[arg_count++] = strtok(token, " ");
+while (arg_count < 63 && args[arg_count - 1] != NULL)
+{
+args[arg_count++] = strtok(NULL, " ");
+}
+args[arg_count] = NULL;  /* Add NULL terminator */
+
+if (execvp(args[0], args) == -1)
+{
+perror("execvp");
+exit(1);  /* Exit child process with error status */
+}
+}
+else /* parent process */
+{
+waitpid(pid, &child_status, 0); /* wait for child to finish */
+if (WIFEXITED(child_status) && WEXITSTATUS(child_status) != 0)
+{
+status = 1; /* set status to 1 if child process fails */
+}
+}
+
+token = strtok_r(NULL, ";", &saveptr); /* move to next token */
+}
+
+return (status);
 }
 
